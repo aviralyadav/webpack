0b8fb878e680b0c78a3b36a9a8ed729d9cdf1a46
Aviral <aviral.sit@gmail.com>
2018-12-31 09:44:55 -0600

    Create ppt.js

---
+++ ppt.js
@@ -0,0 +1,182 @@
+Vision: Internally build a global strategic JPMC events management platform that can support a hybrid of JPMC tools and emerging vendor products.  Be mindful of key stakeholders interests; drive standardized and compliant processes; eliminate vendor dependencies (as targeted); provide long term cost savings and reduce risk through application rationalization.
+
+
+ 
+ 
+ 
+  
+  Limit Risk
+
+
+  
+  
+ 
+
+
+ 
+ 
+ 
+  
+  Limit Risk
+
+
+  
+  
+  §Metron
+  is at end of life – replacing it’s functionality is a priority.
+
+  §Use
+  current technologies, design and development methodologies.
+
+  §Build,
+  test and deliver incrementally.
+
+  
+ 
+ 
+  
+  Think
+  Holistically
+
+
+  
+  
+  §Be inclusive across stakeholder
+  groups; leverage
+  subject matter expertise.
+
+  §Define a target state and ensure
+  delivery moves toward it.
+
+  §Leverage
+  existing successful patterns and approaches.
+
+  §Use and produce re-usable components (API’s).
+
+  
+ 
+ 
+  
+  Reduce
+  Cost & Improve
+  Efficiency
+
+
+  
+  
+  §Rationalize applications as functionality matures in GEM.
+
+  §Automate & Integrate to reduce manual processes.
+
+  §Increase
+  self-service capabilities.
+
+  
+ 
+ 
+  
+  Maintain
+  Flexibility
+
+
+  
+  
+  §Avoid vendor lock-in and vendor specific
+  technologies.
+
+  §Architect
+  the solution such that emerging vendor solutions could be leveraged.
+
+  §Build
+  to support both simple and complex events.
+
+
+ 
+ 
+ 
+  
+  Accelerating Development
+
+
+  
+  
+  §Adopting
+  Architecture and Engineering Cloud best practices and leveraging existing
+  patterns and continuous
+  integration to support end-to-end DevOps.
+
+  
+ 
+ 
+  
+  Adopting
+  Cloud
+
+
+  
+  
+  §GEM will be hosted on GAIA and
+  utilize: Spring Boot; React; Java; Maria DB.
+
+  
+ 
+ 
+  
+  Advancing
+  You
+
+
+  
+  
+  §Using Agile practices to
+  facilitate T-Shaped growth opportunities.
+
+  
+ 
+ 
+  
+  Optimizing
+  Infrastructure
+
+
+  
+  
+  §Deploying to GAIA; targeting
+  decommissioning of existing VSI-Microsoft based (end of life) technology.
+
+  
+ 
+ 
+  
+  Rationalizing
+  Applications
+
+
+  
+  
+  §Replacing
+  end of life current application (Metron) and targeting several
+  others for integration, while identifying potential vendor contract
+  rationalization across SalesForce and Marketo.
+
+  
+ 
+
+  
+ 
+
+
+  
+ Release 1 will focus on building a foundation on which we will be able to incrementally add functionality as the product matures. It will have limited capabilities and risk while allowing us to test basic functionality.
+
+
+Internal event
+Single attendee type
+Registrant is authenticated by SSO – no need to enter name, SID or email
+Generic website banner/template
+Limited/basic information on website
+Option to have a capacity on the event
+Option to have a registration waitlist
+Registrant has ability to add the event to their calendar from the website confirmation page
+Confirmation email sent after registering with a link to add the event to calendar
+Basic RSVP reporting
